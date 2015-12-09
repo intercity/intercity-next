1745fe3d8e7124260aaa6cab6f046e7fd681eeff
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2015-12-09 14:04:17 +0100

    Make sure the deploy key is proparly formatted
    Dokku does not allow deploy keys that are not parameterized. Since we want to
    make sure Dokku gets the proper value, lets parameterize the value in the setter
    
    Fixes: #14
    
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- app/models/deploy_key.rb
+++ app/models/deploy_key.rb
@@ -2,4 +2,8 @@ class DeployKey < ActiveRecord::Base
   belongs_to :server
 
   validates :name, :key, presence: true
+
+  def name=(value)
+    self[:name] = value.parameterize unless value.nil?
+  end
 end
