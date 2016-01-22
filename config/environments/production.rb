f8d4b2c146c76b940a995fe352b0824300dde1d1
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-01-22 14:05:54 +0100

    Use Sendmail in production
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :sendmail
 end
