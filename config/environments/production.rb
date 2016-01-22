53f2c145a4a11c365c6ee2c63bcc2f3ef1404b53
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-01-22 14:44:22 +0100

    Use Hostname
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: ENV["HOSTNAME"] }
 end
