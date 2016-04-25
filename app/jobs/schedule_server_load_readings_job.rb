5b39c4e30d161dc92cab20f64c3be8719395fd14
Bram Bokdam <brambokdam@gmail.com>
2016-04-25 10:57:29 +0200

    Rubcop fix line length

--- app/jobs/schedule_server_load_readings_job.rb
+++ app/jobs/schedule_server_load_readings_job.rb
@@ -3,10 +3,13 @@ class ScheduleServerLoadReadingsJob < ApplicationJob
 
   def perform
     Server.where(status: :up).each do |server|
-      cpu = SshExecution.new(server).execute(command: "uptime | awk '{print $10}' | tr -d '\n'").to_f
+      cpu = SshExecution.new(server).
+            execute(command: "uptime | awk '{print $10}' | tr -d '\n'").to_f
       cpu = 100 if cpu > 100
-      memory = SshExecution.new(server).execute(command: "free | grep Mem | awk '{print $3/$2}' | tr -d '\n'").to_f
-      disk = SshExecution.new(server).execute(command: "df | awk '{print $6,$5/100;}' | grep '^/ ' |  tr -d '\n /%'").to_f
+      memory = SshExecution.new(server).
+               execute(command: "free | grep Mem | awk '{print $3/$2}' | tr -d '\n'").to_f
+      disk = SshExecution.new(server).
+             execute(command: "df | awk '{print $6,$5/100;}' | grep '^/ ' |  tr -d '\n /%'").to_f
 
       ServerLoadReading.create(
         server: server,
