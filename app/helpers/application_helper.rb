646f29965f6818e68bc5e32944c4d4e9bfb6cc05
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-06-03 08:20:02 +0200

    Make sure we load gravatars over HTTPS
    Fixes: #144

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def gravatar_url(email, size: 35)
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=identicon"
+    "https://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=identicon"
   end
 end
