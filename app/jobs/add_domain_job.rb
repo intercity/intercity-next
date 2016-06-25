a90ac5f205a8e633726874178d441546f6e02e91
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-06-25 12:48:02 +0200

    Convert the CreateAppJob and the AddDomainJob to Intercity Runner

--- app/jobs/add_domain_job.rb
+++ app/jobs/add_domain_job.rb
@@ -2,7 +2,9 @@ class AddDomainJob < ActiveJob::Base
   queue_as :default
 
   def perform(app, domain)
-    SshExecution.new(app.server).
-      execute(command: "dokku domains:add #{app.clean_name} '#{domain}'")
+    command = ["domains:add #{app.clean_name} '#{domain}'"]
+    DockerBuilder.new.set_app(app).
+      add_env_var("PLAYBOOK", "execute_dokku.yml").
+      add_encoded_vars("DOKKU_COMMAND", command).run
   end
 end
