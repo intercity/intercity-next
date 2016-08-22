9c08c2162ab8ddc2066475ba22b59f3bfb4e21ca
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-08-22 16:31:18 +0200

    Make rubocop happy

--- app/jobs/rotate_backups_job.rb
+++ app/jobs/rotate_backups_job.rb
@@ -3,7 +3,7 @@ class RotateBackupsJob < ApplicationJob
     backups = Backup.where("created_at < ?", 2.days.ago).where(rotated: false)
 
     backups.each do |backup|
-      if File.exists?(Rails.root.join("backups", backup.app.clean_name, backup.filename))
+      if File.exist?(Rails.root.join("backups", backup.app.clean_name, backup.filename))
         File.delete(Rails.root.join("backups", backup.app.clean_name, backup.filename))
       end
       backup.update(rotated: true, running: false)
