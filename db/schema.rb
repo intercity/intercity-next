ac06a47f418dcad4ac4b848e6a95a6da54225f6a
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-09-02 15:12:12 +0200

    Mark existing linked services as Installed
    We did not keep track of the status of a linked service before. Since we do it
    now, and the default status is 0, all existing services are marked as Not
    Linked. In order to have them show up as linked, we need to update them all.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901100859) do
+ActiveRecord::Schema.define(version: 20160902131047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
