6304cf32f3b328eae5f0a10909bfca52af62ea5a
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2018-01-11 09:31:08 +0100

    Fix typo in production.rb
    When upgrading to Rails 5 I made a mistake in production.rb. This was
    preventing the app to start.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_option = { host: ENV["HOSTNAME"] }
+  config.action_mailer.default_url_options = { host: ENV["HOSTNAME"] }
   config.action_mailer.delivery_method = :smtp
   config.x.default_email_delivery_method = :sendmail
 end
