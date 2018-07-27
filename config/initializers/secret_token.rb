8892ca796408c4bccce92c848fbc740e609b11f2
Ariejan de Vroom <ariejan@devroom.io>
2018-07-27 12:05:26 +0200

    Don't publish secret key to redis

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,12 @@
-require 'securerandom'
-
-$redis ||= Redis.new(url: ENV["REDIS_URL"])
-
-token = $redis.get("SECRET_KEY_BASE")
-if token.nil?
-  token = SecureRandom.hex(64)
-  $redis.set("SECRET_KEY_BASE", token)
-end
-
-Rails.application.config.secret_token = token
-Rails.application.config.secret_key_base = token
+# require 'securerandom'
+#
+# $redis ||= Redis.new(url: ENV["REDIS_URL"])
+#
+# token = $redis.get("SECRET_KEY_BASE")
+# if token.nil?
+#   token = SecureRandom.hex(64)
+#   $redis.set("SECRET_KEY_BASE", token)
+# end
+#
+# Rails.application.config.secret_token = token
+# Rails.application.config.secret_key_base = token
