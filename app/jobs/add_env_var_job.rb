df66f996c6a716f72ae9047b8864fa2dc0104bd6
Michiel Sikkes <michiel@firmhouse.com>
2018-11-19 23:06:18 +0100

    Update AddEnvVarJob to take apply immediately setting into account

--- app/jobs/add_env_var_job.rb
+++ app/jobs/add_env_var_job.rb
@@ -2,7 +2,18 @@ class AddEnvVarJob < ApplicationJob
   queue_as :default
 
   def perform(app, env_var)
-    SshExecution.new(app.server).execute(command: "dokku config:set "\
-                                         "#{app.clean_name} '#{env_var.key}=#{env_var.value}'")
+    @apply_immediately = env_var.apply_immediately
+
+    SshExecution.new(app.server).
+      execute(command: "dokku config:set #{no_restart_argument}"\
+        "#{app.clean_name} '#{env_var.key}=#{env_var.value}'")
+  end
+
+  private
+
+  def no_restart_argument
+    return if @apply_immediately
+
+    "-no-restart "
   end
 end
