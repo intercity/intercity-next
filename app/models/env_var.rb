2144978f4da387c496c6ccb04da3fb42dacf3dec
Michiel Sikkes <michiel@firmhouse.com>
2018-11-20 23:38:32 +0100

    Fix bug in passing apply_immediately setting to AddEnvVarJob
    Make sure to pass apply_immediately into the background job from the controller and don't rely on fetching the setting from the EnvVar model record in the background job.
    
    This because the virtual attribute is not set anymore in the background job when fetching the model record.

--- app/models/env_var.rb
+++ app/models/env_var.rb
@@ -4,4 +4,8 @@ class EnvVar < ApplicationRecord
   validates :key, presence: true, uniqueness: { scope: :app_id }
 
   attr_accessor :apply_immediately
+
+  def apply_immediately?
+    self.apply_immediately == "1"
+  end
 end
