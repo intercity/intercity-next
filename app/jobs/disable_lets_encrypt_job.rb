528cf6cf26b118c0e86748281e7efd5f426f1a2b
Michiel Sikkes <michiel@firmhouse.com>
2018-11-23 21:23:33 +0100

    Set LE enabled column to false in disable job

--- app/jobs/disable_lets_encrypt_job.rb
+++ app/jobs/disable_lets_encrypt_job.rb
@@ -6,5 +6,6 @@ class DisableLetsEncryptJob < ApplicationJob
       execute(command: "sudo dokku config:unset -no-restart #{app.clean_name} DOKKU_LETSENCRYPT_EMAIL")
     SshExecution.new(app.server).
       execute(command: "sudo dokku letsencrypt:revoke #{app.clean_name}")
+    app.update(letsencrypt_enabled: false)
   end
 end
