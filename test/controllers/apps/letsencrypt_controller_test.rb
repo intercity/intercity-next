99f990a3ff8c8defd86fdd992c865d1b84c759cd
Michiel Sikkes <michiel@firmhouse.com>
2018-12-18 22:13:26 +0100

    Mock individual SSH commands to test functionality in the jobo

--- test/controllers/apps/letsencrypt_controller_test.rb
+++ test/controllers/apps/letsencrypt_controller_test.rb
@@ -8,7 +8,13 @@ class Apps::LetsencryptControllerTest < ActionDispatch::IntegrationTest
   test "POST create enables LetsEncrypt" do
     server = servers(:example)
     app = apps(:example)
-    EnableLetsEncryptJob.any_instance.expects(:perform)
+
+    SshExecution.any_instance.expects(:execute).with(command: "sudo dokku plugin").returns("")
+    SshExecution.any_instance.expects(:execute).with(command: "sudo dokku config:get example-app DOKKU_LETSENCRYPT_EMAIL")
+    SshExecution.any_instance.expects(:execute).with(command: "sudo dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git")
+    SshExecution.any_instance.expects(:execute).with(command: "sudo dokku letsencrypt:cron-job --add")
+    SshExecution.any_instance.expects(:execute).with(command: "sudo dokku config:set -no-restart example-app DOKKU_LETSENCRYPT_EMAIL=michiel.sikkes@gmail.com")
+    SshExecution.any_instance.expects(:execute).with(command: "sudo dokku letsencrypt example-app")
 
     post server_app_letsencrypt_url(server,  app), params: { 
       app: {
@@ -16,7 +22,9 @@ class Apps::LetsencryptControllerTest < ActionDispatch::IntegrationTest
       }
     }
 
-    assert_equal "michiel.sikkes@gmail.com", app.reload.letsencrypt_email
+    app.reload
+    assert app.letsencrypt_enabled?
+    assert_equal "michiel.sikkes@gmail.com", app.letsencrypt_email
 
     assert_redirected_to server_app_domains_url(server, app)
   end
@@ -24,7 +32,7 @@ class Apps::LetsencryptControllerTest < ActionDispatch::IntegrationTest
   test "DELETE destroy disables LetsEncrypt" do
     server = servers(:example)
     app = apps(:example)
-    DisableLetsEncryptJob.any_instance.expects(:perform)
+    SshExecution.any_instance.expects(:execute).at_least_once
 
     delete server_app_letsencrypt_url(server,  app)
 
