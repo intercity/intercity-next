f47623d72b2966958073f7eaef1864493b34f024
Michiel Sikkes <michiel@firmhouse.com>
2018-12-18 22:03:21 +0100

    Add a controller test for enabling and disabling LetsEncrypt

--- test/controllers/apps/letsencrypt_controller_test.rb
+++ test/controllers/apps/letsencrypt_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class Apps::LetsencryptControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    login_as(users(:john))
+  end
+
+  test "POST create enables LetsEncrypt" do
+    server = servers(:example)
+    app = apps(:example)
+    EnableLetsEncryptJob.any_instance.expects(:perform)
+
+    post server_app_letsencrypt_url(server,  app), params: { 
+      app: {
+        letsencrypt_email: "michiel.sikkes@gmail.com"
+      }
+    }
+
+    assert_equal "michiel.sikkes@gmail.com", app.reload.letsencrypt_email
+
+    assert_redirected_to server_app_domains_url(server, app)
+  end
+
+  test "DELETE destroy disables LetsEncrypt" do
+    server = servers(:example)
+    app = apps(:example)
+    DisableLetsEncryptJob.any_instance.expects(:perform)
+
+    delete server_app_letsencrypt_url(server,  app)
+
+    assert_redirected_to server_app_domains_url(server, app)
+  end
 end
