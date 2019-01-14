07d1010239613a97d77f42e19a81b2a22e061edc
Michiel Sikkes <michiel@firmhouse.com>
2019-01-14 21:00:39 +0100

    Test that RefreshAppLetsEncryptStatus enables or disables local columns based on server output

--- test/jobs/refresh_app_lets_encrypt_status_job_test.rb
+++ test/jobs/refresh_app_lets_encrypt_status_job_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class RefreshAppLetsEncryptStatusJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#perform should call out to SshExecution to set enabled LetsEncrypt status" do
+    app = apps(:example)
+    app.update(letsencrypt_enabled: false, letsencrypt_email: nil)
+
+    FakeSshExecutioner.return_value = "john@example.com"
+    RefreshAppLetsEncryptStatusJob.perform_now(app)
+
+    app.reload
+    assert app.letsencrypt_enabled?
+    assert_equal "john@example.com", app.letsencrypt_email
+  end
+
+  test "#perform should call out to SshExecution to disabled LetsEncrypt status" do
+    app = apps(:example)
+
+    app.update(letsencrypt_enabled: true, letsencrypt_email: "john@example.com")
+    FakeSshExecutioner.return_value = ""
+
+    RefreshAppLetsEncryptStatusJob.perform_now(app)
+
+    app.reload
+    refute app.letsencrypt_enabled?
+  end
 end
