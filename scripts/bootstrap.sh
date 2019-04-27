03037b19ba5eb3e2255595ed085a4b7245bfebd2
Michiel Sikkes <michiel@firmhouse.com>
2019-04-27 17:04:25 +0200

    Fix URL to docker-compose file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -29,7 +29,7 @@ fi
 # Fetching docker-compose.yml that includes the stack to deploy
 if [ ! -f "./docker-compose.yml" ]; then
   echo "!! No docker-compose.yml yet, fetching from GitHub."
-  curl -L https://raw.githubusercontent.com/intercity/intercity-next/pr/242/docker-compose.yml -o ./docker-compose.yml
+  curl -L https://raw.githubusercontent.com/intercity/intercity-next/master/docker-compose.yml -o ./docker-compose.yml
 else
   echo "-- docker-compose.yml"
 fi
